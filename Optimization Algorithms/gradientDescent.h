47e1320af6e726fb3e3efc95d43fca853d6b19dd
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-01-13 17:30:42 +0330

    Gradient Descent Algorithm completed

--- Optimization Algorithms/gradientDescent.h
+++ Optimization Algorithms/gradientDescent.h
@@ -1,7 +1,5 @@
 #ifndef C_MATH_GRADIENTDESCENT_H
 #define C_MATH_GRADIENTDESCENT_H
-#define INPUT_SIZE 32
-#define DX 1e-6
 
 double gradientDescent(const char *expression, double x0, double ete, double ere, double gamma, int maxiter, int mode,
                        int *state);
