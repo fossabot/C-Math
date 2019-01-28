1e17579aa1e2d90bbe73d59c4983cad2d8ea2f6f
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-01-28 17:04:15 +0330

    random number generator added

---
+++ Source/Assets/Util/randomGenerator.c
@@ -0,0 +1,58 @@
+#include "randomGenerator.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+double zeroToOneUniformRandom(void) {
+    /*
+     * Generate a random number between 0 and 1
+     */
+
+    // set seed
+    seed();
+
+    // generate
+    return rand() / (double) RAND_MAX;
+} // end of zeroToOneUniformRandom
+
+double floatUniformRandom(double a, double b) {
+    /*
+     * Generate a random number between a and b
+     * in real interval
+     */
+
+    // set seed
+    seed();
+
+    // generate
+    return (b - a) * zeroToOneUniformRandom() + a;
+} // end of floatUniformRandom
+
+long int integerUniformRandom(long int n) {
+    /*
+     * Generate a random integer between 1 and given value n
+     */
+
+    // set seed
+    seed();
+
+    // check n
+    if (n < 0) {
+        n = -n;
+    } else if (n == 0) {
+        return 0;
+    }
+
+    // generate
+    long int guard = (long) (zeroToOneUniformRandom() * n) + 1;
+    return (guard > n) ? n : guard;
+} // end of integerUniformRandom
+
+void seed(void) {
+    /*
+     * Reset the random number generator with the system clock
+     */
+
+    time_t t;
+    srand((unsigned) time(&t));
+} // end of seed
---
+++ Source/Assets/Util/randomGenerator.h
@@ -0,0 +1,12 @@
+#ifndef C_MATH_RANDOMGENERATOR_H
+#define C_MATH_RANDOMGENERATOR_H
+
+double zeroToOneUniformRandom(void);
+
+double floatUniformRandom(double a, double b);
+
+long int integerUniformRandom(long int n);
+
+void seed(void);
+
+#endif //C_MATH_RANDOMGENERATOR_H
