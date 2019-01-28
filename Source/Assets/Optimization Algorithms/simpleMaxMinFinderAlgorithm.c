6cfa1ec03b7ec85f4391c6c209a2782a16bf5f8d
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-01-28 16:30:35 +0330

    documented

--- Source/Assets/Optimization Algorithms/simpleMaxMinFinderAlgorithm.c
+++ Source/Assets/Optimization Algorithms/simpleMaxMinFinderAlgorithm.c
@@ -1,13 +1,39 @@
 #include "simpleMaxMinFinderAlgorithm.h"
 #include "../Util/functions.h"
+#include "../Util/util.h"
 
-double *simpleMaxMinFinder(const char *expression, double a, double b, unsigned int steps) {
+#include <stdio.h>
+#include <stdlib.h>
 
+double *simpleMaxMinFinder(const char *expression, double a, double b, unsigned int n) {
+    /*
+     * this function will find global maximum and minimum of a function in interval [a, b]
+     * through sampling y = f(x) and comparing it with previous values
+     *
+     * ARGUMENTS:
+     * expressions   the function expression, it must be a string array like "x^2+1"
+     * a             starting point of interval [a, b]
+     * b             ending point of interval [a, b]
+     * n             number of sub-intervals to use
+     *
+     * RETURN:       a pointer to a double array of size 2
+     *
+     */
+
+    // check n to be more than zero
+    // this is implemented to prevent divide by zero error
+    if (n <= 0) {
+        printf("Error: argument n must be more than zero!\n");
+        Exit();
+        exit(EXIT_FAILURE);
+    } // end of ete check
+
+    // initializing variables
     static double results[2];
-    double coefficient = (b - a) / steps;
+    double coefficient = (b - a) / n;
     double x, y, max = 0, min = 0;
 
-    for (int i = 0; i <= steps; ++i) {
+    for (int i = 0; i <= n; ++i) {
         x = a + coefficient * i;
         y = function_1_arg(expression, x);
 
@@ -21,4 +47,4 @@ double *simpleMaxMinFinder(const char *expression, double a, double b, unsigned 
     results[0] = max;
     results[1] = min;
     return results;
-}
+} // end of simpleMaxMinFinder
--- Source/Assets/Optimization Algorithms/simpleMaxMinFinderAlgorithm.h
+++ Source/Assets/Optimization Algorithms/simpleMaxMinFinderAlgorithm.h
@@ -1,6 +1,6 @@
 #ifndef C_MATH_SIMPLEMAXMINFINDERALGORITHM_H
 #define C_MATH_SIMPLEMAXMINFINDERALGORITHM_H
 
-double *simpleMaxMinFinder(const char *expression, double a, double b, unsigned int steps);
+double *simpleMaxMinFinder(const char *expression, double a, double b, unsigned int n);
 
 #endif //C_MATH_SIMPLEMAXMINFINDERALGORITHM_H
