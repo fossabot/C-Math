d13845ffa9fed047905c5178a8e31ff5d48ab0fc
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-01-31 14:18:16 +0330

    docs updated

--- Source/Assets/Integration Algorithms/trapezoidRuleAlgorithm.c
+++ Source/Assets/Integration Algorithms/trapezoidRuleAlgorithm.c
@@ -7,14 +7,8 @@
 
 double trapezoidRule(const char *expression, double a, double b, unsigned int n, int mode) {
     /*
-     * In mathematics, a Riemann sum is a certain kind of approximation of an integral by a finite sum. It is named
-     * after nineteenth century German mathematician Bernhard Riemann. One very common application is approximating
-     * the area of functions or lines on a graph, but also the length of curves and other approximations.
-     * The sum is calculated by dividing the region up into shapes (rectangles, trapezoids, parabolas, or cubic) that
-     * together form a region that is similar to the region being measured, then calculating the area for each of these
-     * shapes, and finally adding all of these small areas together. This approach can be used to find
-     * a numerical approximation for a definite integral even if the fundamental theorem of calculus
-     * does not make it easy to find a closed-form solution.
+     * In mathematics, and more specifically in numerical analysis, the trapezoidal rule
+     * (also known as the trapezoid rule or trapezium rule) is a technique for approximating the definite integral.
      *
      * ARGUMENTS:
      * expressions   the function expression, it must be a string array like "x^2+1"
