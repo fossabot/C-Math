c1f6faa1a2059e1c6db12eb7829da567491d10d7
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-03-24 18:53:02 +0430

    romberg interface added

--- Source/Integration Algoritms/romberg.c
+++ Source/Integration Algoritms/romberg.c
@@ -1,12 +1,129 @@
 #include "../Assets/Integration Algorithms/rombergAlgorithm.h"
+#include "../Assets/Util/util.h"
+#include "../Assets/Util/_configurations.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    char *expression = "x^3-2*x^2+5";
-    int a = -1, b = 10;
-    unsigned int n = 20;
-    int state = 0;
-    double result = romberg(expression, a, b, n, 1e-6, 0, &state);
-    printf("romberg = %lf\n", result);
-}
+void main() {
+    /*
+     * Interface of program, this interface will get necessary information from user.
+     */
+
+    // initializing variables
+    char expression[INPUT_SIZE];
+    char a[INPUT_SIZE], b[INPUT_SIZE], k_c[INPUT_SIZE], tol_c[INPUT_SIZE], verbose_c[INPUT_SIZE],
+            tryAgain_c[INPUT_SIZE];
+    char *ptr;
+    int verbose = 0, k = 0, tryAgain = 0;
+    double a0, b0, tol;
+
+    printf("\t\t\t\tIntegral Calculator\n"
+           "\t\t\t\t     Romberg\n");
+
+    START: //LABEL for goto
+    // getting required data from user
+    printf("\nEnter the function you want to integrate (example: x^2-3):\n");
+    fgets(expression, sizeof(expression), stdin);
+
+    INTERVAL: //LABEL for goto
+    printf("Choose an interval [a, b]:\n");
+    printf("Enter a:\n");
+    fgets(a, sizeof(a), stdin);
+    a0 = strtod(a, &ptr);
+    printf("Enter b:\n");
+    fgets(b, sizeof(b), stdin);
+    b0 = strtod(b, &ptr);
+
+    // check interval
+    if (a0 == b0) {
+        printf("Error: improper interval! 'a' and 'b' can't have same values.\n");
+
+        // a chance to correct your mistake :)
+        printf("\nDo you want to try again? {0: no, 1: yes}\n");
+        fgets(tryAgain_c, sizeof(tryAgain_c), stdin);
+        tryAgain = strtol(tryAgain_c, &ptr, 10);
+        if (tryAgain) {
+            goto INTERVAL;
+        } else {
+            Exit(EXIT_FAILURE);
+        } // end of if goto
+    } //end of interval check
+
+    K_NUMBER: //LABEL for goto
+    printf("Enter degree (k) of integration\n");
+    // get number from user
+    fgets(k_c, sizeof(k_c), stdin);
+    k = strtol(k_c, &ptr, 10);
+
+    // check k to be positive
+    if (k < 0) {
+        printf("Error: k can't be less than 0.\n");
+        // a chance to correct your mistake :)
+        printf("\nDo you want to try again? {0: no, 1: yes}\n");
+        fgets(tryAgain_c, sizeof(tryAgain_c), stdin);
+        tryAgain = strtol(tryAgain_c, &ptr, 10);
+        if (tryAgain) {
+            goto K_NUMBER;
+        } else {
+            Exit(EXIT_FAILURE);
+        } // end of if goto
+    } // end of ete check
+
+    TOL: //LABEL for goto
+    printf("Enter the tolerance limit (enter 0 if you don't want to set a tolerance limit):\n");
+    fgets(tol_c, sizeof(tol_c), stdin);
+    tol = strtod(tol_c, &ptr);
+
+    // check tol to be positive
+    if (tol < 0) {
+        printf("Error: estimated tolerance limit must be a \"POSITIVE\" number!\n");
+
+        // a chance to correct your mistake :)
+        printf("\nDo you want to try again? {0: no, 1: yes}\n");
+        fgets(tryAgain_c, sizeof(tryAgain_c), stdin);
+        tryAgain = strtol(tryAgain_c, &ptr, 10);
+        if (tryAgain) {
+            goto TOL;
+        } else {
+            Exit(EXIT_FAILURE);
+        } // end of if goto
+    } // end of ere check
+
+    VERBOSE: //LABEL for goto
+    printf("Do you want to see steps? {0: no, 1: yes}:\n");
+    fgets(verbose_c, sizeof(verbose_c), stdin);
+    verbose = strtol(verbose_c, &ptr, 10);
+
+    // check verbose value
+    if (verbose != 0 && verbose != 1) {
+        printf("Error: invalid value for verbose!\n");
+
+        // a chance to correct your mistake :)
+        printf("\nDo you want to try again? {0: no, 1: yes}\n");
+        fgets(tryAgain_c, sizeof(tryAgain_c), stdin);
+        tryAgain = strtol(tryAgain_c, &ptr, 10);
+        if (tryAgain) {
+            goto VERBOSE;
+        } else {
+            Exit(EXIT_FAILURE);
+        } // end of if goto
+    } // end of if verbose
+
+    // calculation
+    double area = romberg(expression, a0, b0, (unsigned int) k, tol, verbose);
+
+    // show result
+    printf("\nEstimated area under the function %sin the interval [%lf, %lf] is equal to: %lf.\n\n", expression,
+           a0, b0, area);
+
+    // do you want to start again??
+    printf("\nDo you want to start again? {0: no, 1: yes}\n");
+    fgets(tryAgain_c, sizeof(tryAgain_c), stdin);
+    tryAgain = strtol(tryAgain_c, &ptr, 10);
+    if (tryAgain) {
+        goto START;
+    } else {
+        Exit(EXIT_SUCCESS);
+    } // end of if goto
+} // end of main
