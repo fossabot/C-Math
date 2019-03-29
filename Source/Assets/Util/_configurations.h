a61ee9569dd95429d4e07e809365a0abebaf210b
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-03-29 15:58:49 +0430

    module updated
    defined new constans to handle success/failure flags
    
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- Source/Assets/Util/_configurations.h
+++ Source/Assets/Util/_configurations.h
@@ -3,6 +3,9 @@
 
 #define INPUT_SIZE 32
 
+#define HAS_A_ROOT 1
+#define HAS_NO_ROOT 0
+
 #define DX 1e-8 // for computing derivatives up to second grade
 #define DX_SECOND 1e-4 // for computing derivatives up to forth grade
 #define DX_THIRD 1e-2 // for computing derivatives up to sixth grade
