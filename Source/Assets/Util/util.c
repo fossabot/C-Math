f207cdbbfe60b6dc2558c69c37ab6a0323dd5f3e
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-03-29 15:19:20 +0430

    module updated
    added some swap functions for various types, and a general swap macro for all types

--- Source/Assets/Util/util.c
+++ Source/Assets/Util/util.c
@@ -1,15 +1,51 @@
 #include "util.h"
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// common swap functions
+void swapSizeT(size_t *a, size_t *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+} // end of swapSizeT function
+
+void swapSInt(short int *a, short int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+} // end of swapSInt function
+
+void swapInt(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+} // end of swapInt function
+
+void swapLInt(long int *a, long int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+} // end of swapLint function
+
+void swapFloat(float *a, float *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+} // end of swapFloat function
 
 void swapDouble(double *a, double *b) {
     *a = *a + *b;
     *b = *a - *b;
     *a = *a - *b;
-} // end of swap function
+} // end of swapDouble function
 
+// exit function for c-math
 void Exit(int exitCode) {
+    /*
+     * This function's jop is to prevent instant terminal exit
+     * it will ask user to press any key and then will exit
+     * the program with the exit code provided by caller function
+     */
     printf("\nPress any key to exit ...\n");
     getc(stdin);
     exit(exitCode);
--- Source/Assets/Util/util.h
+++ Source/Assets/Util/util.h
@@ -1,8 +1,28 @@
 #ifndef C_MATH_UTIL_H
 #define C_MATH_UTIL_H
 
+#include <stdlib.h>
+
+// this macro will swap anything, regardless of it's type
+// care must be taken while using this macro
+#define EZ_SWAP(a, b) {(a) = (a) + (b); (b) = (a) - (b); (a) = (a) - (b);};
+
+
+// common swap functions
+void swapSizeT(size_t *a, size_t *b);
+
+void swapSInt(short int *a, short int *b);
+
+void swapInt(int *a, int *b);
+
+void swapLInt(long int *a, long int *b);
+
+void swapFloat(float *a, float *b);
+
 void swapDouble(double *a, double *b);
 
+
+// exit function for c-math
 void Exit(int exitCode);
 
 #endif //C_MATH_UTIL_H
