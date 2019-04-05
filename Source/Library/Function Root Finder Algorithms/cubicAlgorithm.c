3430742d8a9af1064dac7c46d1cbfc576f7cfe50
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-04-05 15:40:42 +0430

    module completed
    cubic root finder added, flags has been updated for cubic return states.
    
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- Source/Library/Function Root Finder Algorithms/cubicAlgorithm.c
+++ Source/Library/Function Root Finder Algorithms/cubicAlgorithm.c
@@ -133,7 +133,7 @@ double *asl_cubic_real_root(double a, double b, double c, double d, int verbose,
     // Handle quadratic and linear cases
     if (a == 0) {
         if (verbose == ASL_VERBOSE) {
-            printf("Function is quadratic, it has two possible roots.\n");
+            printf("Function could be quadratic because a = 0, it might have two possible roots.\n");
         } // end if(verbose == ASL_VERBOSE)
 
         // use quadratic solver instead
@@ -142,6 +142,195 @@ double *asl_cubic_real_root(double a, double b, double c, double d, int verbose,
         result[0] = quadratic_result[0];
         result[1] = quadratic_result[1];
 
-    } // end of if
+        return result;
+    } else {
+        // if a is non-zero, normalize the function to
+        // the standard form "x^3 + b x^2 + c x + d = 0"
+        // by dividing all the constants by a.
+        b /= a;
+        c /= a;
+        d /= a;
+    } // end of if else
 
+    // q = Δ0, r = Δ1
+    double q = (b * b - 3 * c);
+    double r = (2 * b * b * b - 9 * b * c + 27 * d);
+
+    if (verbose == ASL_VERBOSE) {
+        printf("First, we calculate Δ0 and Δ1: Δ0 = %g, Δ1 = %g .\n"
+               "we define Δ with this equality: Δ1^2 − 4*Δ0^2 = −27* Δ  .\n", q, r);
+    } // end if(verbose == ASL_VERBOSE)
+
+    double Q = q / 9;
+    double R = r / 54;
+
+    if (verbose == ASL_VERBOSE) {
+        printf("Then, we define the intermediate variables: Q = Δ0/9 = %g, R = Δ1/54 = %g .\n"
+               "The general cubic equation then becomes: x^3 - 3Qx + 2R = 0 .\n", Q, R);
+    } // end if(verbose == ASL_VERBOSE)
+
+    double Q3 = Q * Q * Q;
+    double R2 = R * R;
+
+    // these variables are basically Q3 and R2 but written
+    // in a form suitable for exact computation with integers
+    double CQ3 = 2916 * q * q * q;
+    double CR2 = 729 * r * r;
+
+    if (q == 0 && r == 0) {
+        if (verbose == ASL_VERBOSE) {
+            printf("Δ = 0 and Δ0 = 0, then the equation has a single 'real' root (which is a triple root).\n");
+        } // end if(verbose == ASL_VERBOSE)
+
+        *state = ASL_HAS_TRIPLE_ROOTS;
+
+        x1 = -b / 3;
+        result[0] = x1;
+        result[1] = x1;
+        result[2] = x1;
+
+        return result;
+    } else if (CR2 == CQ3) {
+        /*
+         * this test is actually R2 == Q3, written in a form suitable
+         * for exact computation with integers
+         *
+         * Due to finite precision some double roots may be missed, and
+         * considered to be a pair of complex roots z = x +/- epsilon i
+         * close to the real axis.
+         *
+         */
+
+        if (verbose == ASL_VERBOSE) {
+            printf("Δ = 0 and Δ0 ≠ 0, then there are both a double 'real' root, and a simple 'real' root.\n");
+        } // end if(verbose == ASL_VERBOSE)
+
+        double sqrtQ = sqrt(Q);
+
+        if (R > 0) {
+            x1 = -2 * sqrtQ - b / 3;
+            x2 = sqrtQ - b / 3;
+            result[0] = x1;
+            result[1] = x2;
+            result[2] = x2;
+        } else {
+            x1 = -sqrtQ - b / 3;
+            x2 = 2 * sqrtQ - b / 3;
+            result[0] = x2;
+            result[1] = x2;
+            result[2] = x1;
+        } // end of if
+
+        *state = ASL_HAS_DOUBLE_SIMPLE_ROOTS;
+
+        return result;
+    } else if (R2 < Q3) {
+        /*
+         * Trigonometric solution for three real roots
+         *
+         * When a cubic equation has three real roots, the formulas expressing these roots in terms of radicals
+         * involve complex numbers. It has been proved that when none of the three real roots is rational
+         * —the casus irreducibilis— one cannot express the roots in terms of real radicals. Nevertheless,
+         * purely real expressions of the solutions may be obtained using hypergeometric functions, or
+         * more elementarily in terms of trigonometric functions, specifically in terms of the cosine and arccosine
+         * functions.
+         *
+         * The formulas which follow, due to François Viète, are true in general (except when p = 0), and
+         * are purely real when the equation has three real roots, but involve complex cosines and arccosines
+         * when there is only one real root.
+         *
+         * define:
+         * x = t - b/3
+         * p = (3ac − b^2) / (3 * a^2)
+         * q = (2*b^3 − 9abc + 27*a^2*d) / (27*a^3)
+         *
+         * the roots are:
+         * t(k) = 2*sqrt( −p/3 ) * cos( [arccos( 3q/2p − sqrt(-3/p) ) − (2 π k)] / 3 ) for k = 0 , 1 , 2 .
+         *
+         * calculation in this program is done by below equivalents:
+         * 2*sqrt( −p/3 ) = -2 * sqrt(Q) ==> norm
+         * 3q/2p − sqrt(-3/p) = sgnR * sqrt(R2 / Q3) ==> ratio
+         * arccos( 3q/2p − sqrt(-3/p) ) = acos(ratio) ==> theta
+         *
+         */
+
+        if (verbose == ASL_VERBOSE) {
+            printf("Δ > 0, then there are three distinct 'real' roots.\n");
+        } // end if(verbose == ASL_VERBOSE)
+
+        double sgnR = (R >= 0 ? 1 : -1);
+        double ratio = sgnR * sqrt(R2 / Q3);
+        double theta = acos(ratio);
+        double norm = -2 * sqrt(Q);
+
+        x1 = norm * cos(theta / 3) - b / 3;
+        x2 = norm * cos((theta + 2.0 * M_PI) / 3) - b / 3;
+        x3 = norm * cos((theta - 2.0 * M_PI) / 3) - b / 3;
+
+        // Sort x1, x2, x3 into increasing order
+
+        if (x1 > x2) {
+            swapDouble(&x1, &x2);
+        } // end of if
+
+        if (x2 > x3) {
+            swapDouble(&x2, &x3);
+
+            if (x1 > x2) {
+                swapDouble(&x1, &x2);
+            } // end of if
+        } // end of if
+
+        *state = ASL_HAS_THREE_ROOTS;
+
+        result[0] = x1;
+        result[1] = x2;
+        result[2] = x3;
+
+        return result;
+    } else {
+        /*
+         *  Defining:
+         *  D	=	R^2 - Q^3
+         *  S	=	cubeRoot(R + sqrt(D))
+         *  T	=	cubeRoot(R - sqrt(D))
+         *
+         *  where D is the polynomial discriminant (which is defined slightly differently,
+         *  including the opposite sign, by Birkhoff and Mac Lane 1996) then gives
+         *  very simple expressions for A and B, namely
+         *
+         *  A	=	S-T
+         *  B	=	S+T
+         *
+         *  Therefore, at last, the roots of the original equation "x^3 + b*x^2 + cx + d = 0"
+         *  in z are then given by:
+         *  z1	=	-b/3 + (S+T)                            "real root"
+         *  z2	=	-b/3 - [(S+T) + i*sqrt(3)(S-T)] / 2     "complex root"
+         *  z3	=	-b/3 - [(S+T) - i*sqrt(3)(S-T)] / 2     "complex root"
+         *
+         *  in this function we are only seeking the real root, x = -b/3 + (S+T)
+         *
+         *  calculation in this program is done by below equivalents:
+         *  S = cubeRoot(R + sqrt(D)) = -sgnR * pow(fabs(R) + sqrt(R2 - Q3), 1.0 / 3.0) ==> S
+         *  T =	cubeRoot(R - sqrt(D)) = T = Q / S ==> T
+         *
+         */
+
+        if (verbose == ASL_VERBOSE) {
+            printf("Δ < 0, then there are one 'real' root and two complex roots.\n"
+                   "however this function only computes real root, and not the complex ones.\n");
+        } // end if(verbose == ASL_VERBOSE)
+
+        double sgnR = (R >= 0 ? 1 : -1);
+        double S = -sgnR * pow(fabs(R) + sqrt(R2 - Q3), 1.0 / 3.0);
+        double T = Q / S;
+
+        x1 = S + T - b / 3;
+
+        *state = ASL_HAS_A_ROOT;
+
+        result[0] = x1;
+
+        return result;
+    } // end of if else
 } // end of asl_cubic_real_root
